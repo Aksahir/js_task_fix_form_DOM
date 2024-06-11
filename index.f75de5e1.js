a2f0332b933e6f22514acf2126115f2a17bafd13
Александр <0999353523ak@gmail.com>
2024-06-11 16:16:57 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,16 @@
+"use strict";
+const inputs = [
+    ...document.querySelectorAll("form input")
+];
+inputs.forEach((input)=>{
+    const label = document.createElement("label");
+    const regex = /(?=[A-Z])/;
+    const textForLable = input.name.split(regex).map((word)=>word.toUpperCase()).join(" ");
+    label.className = "field-label";
+    label.setAttribute("for", input.id);
+    input.setAttribute("placeholder", textForLable);
+    label.textContent = textForLable;
+    input.parentElement.insertBefore(label, input);
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
